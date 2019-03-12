a9fd6259e5ebc82f6ca50025f7c0e267643b327d
= <=>
2019-03-12 22:40:13 +0700

    a

---
+++ Tài liệu/8.10/chuabai6/1.js
@@ -0,0 +1,27 @@
+﻿ $(function(){
+	 console.log('tada ! ');
+
+	 $(window).scroll(function(){
+ 	 	vitrihientai = $('body').scrollTop();
+	 	console.log(vitrihientai);
+
+	 	if(vitrihientai >337){
+	 		$('.navbar-fixed-top').addClass('tienhoa');
+	 	} 
+	 	else if(vitrihientai <337){
+	 		$('.navbar-fixed-top').removeClass('tienhoa');
+	 	}
+
+
+	 	// mo rong 
+	 	if(vitrihientai > 500) {
+	 		$('.portfolio').addClass('bienra');
+	 	}
+	 	else if(vitrihientai < 700) {
+	 		$('.portfolio').removeClass('bienra');
+	 	}
+
+	 })
+})  
+	 
+     
---
+++ Tài liệu/8.12/chuabai6/1.js
@@ -0,0 +1,47 @@
+﻿ $(function(){
+	 console.log('tada ! ');
+	 // ham click 
+	 $('.n1').click(function(){
+	 	$('body').animate({scrollTop:$('#sanpham').offset().top},2000,"easeInOutExpo");
+	 	return false; 
+	 })
+$('.n2').click(function(){
+	 	$('body').animate({scrollTop:$('#gioithieu').offset().top},2000,"easeInOutExpo");
+	 	return false; 
+	 })
+
+
+$('.n3').click(function(){
+	 	$('body').animate({scrollTop:$('#lienhe').offset().top},2000,"easeInOutExpo");
+	 	return false; 
+	 })
+
+
+
+
+
+
+	 $(window).scroll(function(){
+ 	 	vitrihientai = $('body').scrollTop();
+	 	console.log(vitrihientai);
+
+	 	if(vitrihientai >337){
+	 		$('.navbar-fixed-top').addClass('tienhoa');
+	 	} 
+	 	else if(vitrihientai <337){
+	 		$('.navbar-fixed-top').removeClass('tienhoa');
+	 	}
+
+
+	 	// mo rong 
+	 	if(vitrihientai > 500) {
+	 		$('.portfolio').addClass('bienra');
+	 	}
+	 	else if(vitrihientai < 700) {
+	 		$('.portfolio').removeClass('bienra');
+	 	}
+
+	 })
+})  
+	 
+     
